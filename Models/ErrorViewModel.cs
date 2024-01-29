1a55051402015f958494633f5aa1b980c623c544
Hakan Sait Bozkurt <b.hakan@palladium.gen.tr>
2024-01-29 15:17:56 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp_dotnet_core_calculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
